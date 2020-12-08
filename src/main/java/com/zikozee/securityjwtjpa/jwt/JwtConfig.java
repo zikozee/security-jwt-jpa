0f8ca15dc1153848500c44415e6f813a5acee68f
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-12-08 14:19:44 +0100

    Type-safe Configuration Properties (Instead of @Value). closes #7

--- src/main/java/com/zikozee/securityjwtjpa/jwt/JwtConfig.java
+++ src/main/java/com/zikozee/securityjwtjpa/jwt/JwtConfig.java
@@ -5,20 +5,17 @@ import com.google.common.net.HttpHeaders;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "application.jwt") //read https://docs.spring.io/spring-boot/docs/2.3.3.RELEASE/reference/html/appendix-configuration-metadata.html#configuration-metadata-annotation-processor
+@ConfigurationProperties("application.jwt") //read https://docs.spring.io/spring-boot/docs/2.3.3.RELEASE/reference/html/appendix-configuration-metadata.html#configuration-metadata-annotation-processor
 @NoArgsConstructor @Getter @Setter
 public class JwtConfig {
 
     private String secretKey;
     private String tokenPrefix;
-//    private Integer tokenExpirationAfterDays;
-    @Value("${application.jwt.tokenExpirationAfterSeconds}")
-    private Integer tokenExpirationAfterSeconds;
+    private Integer tokenExpirationAfterSeconds;// now works //read https://docs.spring.io/spring-boot/docs/2.1.13.RELEASE/reference/html/boot-features-external-config.html#boot-features-external-config-typesafe-configuration-properties
 
 
     public String getAuthorizationHeader(){
